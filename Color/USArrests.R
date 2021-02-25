40ea0ea46dfe52b3270c909bb2d8210fb0c2e8f5
TobyJChappell <tchappell@chapman.edu>
2021-02-25 10:03:53 -0800

    Update

--- Color/USArrests.R
+++ Color/USArrests.R
@@ -6,11 +6,22 @@ library(viridis)
 library(maps) # for the state map data
 library(mapproj)
 
+murder_rates <- read.csv(file = 'MurderRate.csv')
+murder_rates$state <- tolower(murder_rates$state)
 states_map<-map_data("state") #extracts data from the states map
 crimes<-data.frame(state=tolower(rownames(USArrests)),USArrests)
+crimes<-merge(crimes,murder_rates,by="state")
 crime_map<-merge(states_map,crimes,by.x="region",by.y="state")
 crime_map<-arrange(crime_map,group,order)
+c = hsv((max(crime_map$Murder)-crime_map$Murder)/(max(crime_map$Murder)-min(crime_map$Murder))/7,(crime_map$rate-min(crime_map$rate))/(max(crime_map$rate)-min(crime_map$rate)),.95)
 
+#Hue determined by Murder Arrest Rate and Saturation determined by Murder Rate
 ggplot(crime_map,aes(x=long,y=lat,group=group)) + 
   coord_map("polyconic") + 
-  geom_polygon(fill=hsv((max(crime_map$Murder)-crime_map$Murder)/(max(crime_map$Murder)-min(crime_map$Murder))/7,(crime_map$Murder-min(crime_map$Murder))/(max(crime_map$Murder)-min(crime_map$Murder)),1))
+  geom_polygon(fill=c)
+
+#Hue determined by Murder Arrest Rate and Brightness determined by Murder Rate
+ggplot(crime_map) +
+  coord_map("polyconic") + 
+  geom_polygon(aes(x=long, y=lat, group=group, fill=Murder)) +
+  geom_polygon(aes(x=long, y=lat, group=group, alpha=rate))
--- Color_Wheel/Color_Wheel.R
+++ Color_Wheel/Color_Wheel.R
@@ -1,6 +1,5 @@
 library(ggplot2)
 d=expand.grid(h=seq(0,1,1/11.9), s=seq(.2,1,1/7), v=seq(.2,1,1/7))
-#d=expand.grid(h=seq(0,1,1/11.9), s=c(0,1/3,2/3,1,1,1,1,1), v=c(1,1,1,1,1,2/3,1/3,0))
 
 ggplot() +
   coord_polar(theta="x") +
@@ -15,13 +14,3 @@ ggplot() +
   scale_y_continuous(breaks=NULL) +
   scale_fill_identity() +
   geom_rect(data=d, mapping=aes(xmin=h, xmax=h+resolution(h), ymin=v, ymax=v+resolution(v), fill=hsv(h,s,v)))
-
-library(ggplot2)
-d=expand.grid(h=seq(0,0.95,0.05), s=seq(0,0.95,0.05), v=seq(0,1,0.2))
-ggplot() +
-  coord_polar(theta="x") +
-  facet_wrap(~v) +
-  scale_x_continuous(name="hue", limits=c(0,1), breaks=seq(0.025,0.925,0.1), labels=seq(0,0.9,0.1)) +
-  scale_y_continuous(name="saturation", breaks=seq(0, 1, 0.2)) +
-  scale_fill_identity() +
-  geom_rect(data=d, mapping=aes(xmin=h, xmax=h+resolution(h), ymin=s, ymax=s+resolution(s), fill=hsv(h,s,v)), color="white", size=0.1)
